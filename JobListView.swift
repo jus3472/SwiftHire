97fe8bb1c7e02d598cbe415bde1ac75d82ad2343
Justin Jiang <jus3472@gmail.com>
2024-06-27 00:17:12 -0400

    incorporated ZStack and HStack to hide the default arrow from NavigationLink, so I can add a custom arrow instead to improve the UI.

--- JobListView.swift
+++ JobListView.swift
@@ -13,45 +13,52 @@ struct JobListView: View {
 
     var body: some View {
         NavigationView {
-            List(viewModel.filteredJobs) { job in    // list displaying filtered job entries
-                NavigationLink(destination: JobDetailView(job: job)) {
-                        VStack(alignment: .leading, spacing: 8) {
-                            
-                            Text(job.jobTitle).font(.custom("Vedo", size: 18))
-                                .fontWeight(.heavy)
-                                .foregroundColor(Color(red: 61/255, green: 61/255, blue: 61/255, opacity: 1)).padding(.top, 9)
-                            
-                            Text(job.companyName).font(.custom("AvenirNext-Medium", size: 16))
-                                .foregroundColor(Color(red: 244/255, green: 96/255, blue: 54/255, opacity: 1))
-                            
-                            Text(job.location).font(.custom("AvenirNext-Regular", size: 12))
-                                .foregroundColor(.gray).frame(width: 100, height: 10)
+            List(viewModel.filteredJobs) { job in
+                ZStack {
+                    // invisible NavigationLink
+                    NavigationLink(destination: JobDetailView(job: job)) {
+                        EmptyView()
+                    }
+                    .opacity(0) // makes the NavigationLink and its default pointer fully transparent
 
+                    HStack {
+                        VStack(alignment: .leading, spacing: 8) {
+                            Text(job.jobTitle)
+                                .font(.custom("Vedo", size: 18))
+                                .fontWeight(.heavy)
+                                .foregroundColor(Color(red: 61/255, green: 61/255, blue: 61/255))
+                                .padding(.top, 9)
+                            Text(job.companyName)
+                                .font(.custom("AvenirNext-Medium", size: 16))
+                                .foregroundColor(Color(red: 244/255, green: 96/255, blue: 54/255))
+                            Text(job.location)
+                                .font(.custom("AvenirNext-Regular", size: 12))
+                                .foregroundColor(.gray)
+                                .frame(width: 100, height: 10)
                         }
-                    
+                        Spacer()
+                        Image(systemName: "arrowshape.right.fill")
+                            .foregroundColor(Color(red: 244/255, green: 96/255, blue: 54/255))
+                            .brightness(0.1)
+                    }
+                    .padding(5)
                 }
             }
             
-            // adds search functionality
             .searchable(text: $viewModel.searchText)
-            
-            // alert setup, shown based on showAlert state
             .alert(isPresented: $showAlert, content: {
                 Alert(title: Text("Error"), message: Text(viewModel.errorMessage ?? "Unknown error"), dismissButton: .default(Text("OK")) {
-                    viewModel.errorMessage = nil    // clear error message on dismiss
-                    showAlert = false   // hide alert
+                    viewModel.errorMessage = nil    // Clear error message on dismiss
+                    showAlert = false   // Hide alert
                 })
             })
             .onAppear {
-                // show alert if there is an error message
                 showAlert = viewModel.errorMessage != nil
-                // loads jobs when view appears
                 viewModel.loadJobs()
             }
-            .navigationTitle("Jobs").font(.custom("Futura-Medium", size: 15))
+            .navigationTitle("Jobs")
             .listStyle(GroupedListStyle())
         }
-        
     }
 }
 
